eebf33b2b6fb3777f8693d6d69d165412c6b6730
cagri <cagri.uysal@trendyol.com>
2024-03-22 21:54:53 +0300

    feat: add queue

---
+++ src/queue/queue.go
@@ -0,0 +1,21 @@
+package queue
+
+import "errors"
+
+type Queue struct {
+	items []int
+}
+
+func (q *Queue) Enqueue(item int) {
+	q.items = append(q.items, item)
+}
+
+func (q *Queue) Dequeue() (int, error) {
+	if len(q.items) == 0 {
+		return 0, errors.New("queue underflow")
+	}
+
+	item := q.items[0]
+	q.items = q.items[1:]
+	return item, nil
+}
---
+++ src/queue/queue_test.go
@@ -0,0 +1,25 @@
+package queue_test
+
+import (
+	"dsa/src/queue"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestQueue(t *testing.T) {
+	q := queue.Queue{}
+
+	q.Enqueue(1)
+	q.Enqueue(2)
+	item, _ := q.Dequeue()
+
+	if diff := cmp.Diff(1, item); diff != "" {
+		t.Error(diff)
+	}
+
+	item, _ = q.Dequeue()
+	if diff := cmp.Diff(2, item); diff != "" {
+		t.Error(diff)
+	}
+}
